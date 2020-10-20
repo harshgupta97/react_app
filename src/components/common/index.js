0ecaee533bb4e5e259b1348140c4d3e6e7d3a3c7
Harsh Gupta <warmachine476@gmail.com>
2020-10-20 22:10:00 +0530

    Added BottomNav and AppNavButton to component/common index

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,13 +1,17 @@
 import AppHeader from './Header';
 import AppFooter from './Footer';
+import AppBottomNav from './BottomNav';
 import AppCard from './Card';
 import AppChart from './Chart';
 import AppButton from './Button';
+import AppNavButton from './NavButton';
 
 export {
     AppHeader,
     AppFooter,
+    AppBottomNav,
     AppCard,
     AppChart,
     AppButton,
+    AppNavButton,
 }
