94d89863659c3763580d80173afd8477c43764fa
Harsh Gupta <warmachine476@gmail.com>
2020-10-20 23:08:47 +0530

    Finally - import Provider class from react-redux, Switch and Route from react-router-dom, AppHeader and AppBottomNav   from common component and screens with respect to their route.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import { AppHeader, AppBottomNav } from './components/common';
+import { Bills, Home, Invoices, Settings, Items, Reports, Payments } from './screens';
+
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello World</p>
+    <div>
+      <Provider store={store}>
+        <div className="fixed-top">
+          <AppHeader />
+        </div>
+        <div className="mt-8">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/bills">
+              <Bills />
+            </Route>
+            <Route path="/invoices">
+              <Invoices />
+            </Route>
+            <Route path="/settings">
+              <Settings />
+            </Route>
+            <Route path="/payment">
+              <Payments />
+            </Route>
+            <Route path="/items">
+              <Items />
+            </Route>
+            <Route path="/report">
+              <Reports />
+            </Route>
+          </Switch>
+        </div>
+        <AppBottomNav />
+      </Provider>
     </div>
   );
 }
