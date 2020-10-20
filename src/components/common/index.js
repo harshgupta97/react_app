20bae6cb6922dd82a9b40f8dd2d03bcdb56627b2
Harsh Gupta <warmachine476@gmail.com>
2020-10-20 21:49:31 +0530

    Component index under common

---
+++ src/components/common/index.js
@@ -0,0 +1,13 @@
+import AppHeader from './Header';
+import AppFooter from './Footer';
+import AppCard from './Card';
+import AppChart from './Chart';
+import AppButton from './Button';
+
+export {
+    AppHeader,
+    AppFooter,
+    AppCard,
+    AppChart,
+    AppButton,
+}
