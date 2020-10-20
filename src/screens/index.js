8a8e84e5ca4599989e6feacd57004137a6c6d744
Harsh Gupta <warmachine476@gmail.com>
2020-10-20 22:56:29 +0530

    Imported all the screen to screen index

---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import Bills from './Bills';
+import Home from './Home';
+import Invoices from './Invoices';
+import Settings from './Settings';
+import Items from './Items';
+import Reports from './Reports';
+import Payments from './Payments';
+
+export { Bills, Home, Invoices, Settings, Items, Reports, Payments }
