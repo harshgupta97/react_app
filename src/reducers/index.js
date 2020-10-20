afe64004615950f14c3b658af698f3f6d4889471
Harsh Gupta <warmachine476@gmail.com>
2020-10-20 21:57:34 +0530

    imported navReducer to reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
 
+import navReducer from './navReducer';
+
 export default combineReducers({
+    nav: navReducer
     // Create more reducers, import and combine them here
 })
