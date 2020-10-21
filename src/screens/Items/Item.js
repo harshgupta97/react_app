edaccb35b1483a80a0c138ebf2f5726d9a667920
Harsh Gupta <warmachine476@gmail.com>
2020-10-21 13:37:07 +0530

    Created item class, Passing that in ItemScreenView inside renderItem method

---
+++ src/screens/Items/Item.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { AppCard } from '../../components/common';
+import { Link } from 'react-router-dom';
+import { EditOutlined } from '@ant-design/icons';
+import { Row, Col, Typography, Tag, Divider } from 'antd';
+
+const { Title, Text } = Typography;
+
+export default class Item extends Component {
+    render() {
+        const { item } = this.props;
+        return (
+            <div>
+                <Row className="my-2">
+                    <Col span={24} xl={6} xs={24} md={12}>
+                        <AppCard>
+                            <div className="d-flex justify-content-between">
+                                <Title level={4}>{item.title} <Tag color={item.tag === 'Product' ? 'purple' : 'blue'}>{item.tag}</Tag></Title>
+                                <Link to={''}><EditOutlined /></Link>
+                            </div>
+                            <Text type="secondary">SKU: {item.SKU}</Text>
+                            <Divider />
+                            <Row>
+                                <Col span={12}>
+                                    <Text type="secondary">HSN Number</Text><br />
+                                    <Text type="secondary">{item.HSN}</Text>
+                                    <div className="vl"></div>
+                                </Col>
+                                <Col span={12}>
+                                    <Text type="secondary">Item Price</Text><br />
+                                    <Text type="secondary">{item.price_fmt}</Text>
+                                </Col>
+                            </Row>
+                            <Divider dashed={true} />
+                            <Row>
+                                <Col span={8}>
+                                    <Text type="secondary">Tax Rate</Text><br />
+                                    <Text type="secondary">{item.taxRate}{item.ch}</Text>
+                                </Col>
+                                <Col span={8}>
+                                    <Text type="secondary">Unit</Text><br />
+                                    <Text type="secondary">{item.unit}</Text>
+                                </Col>
+                                <Col span={8}>
+                                    <Text type="secondary">Initial Stock</Text><br />
+                                    <Text type="secondary">{item.initialStock}</Text>
+                                </Col>
+                            </Row>
+                        </AppCard>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
--- src/screens/Items/ItemsScreenView.js
+++ src/screens/Items/ItemsScreenView.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react'
-import { AppCard } from '../../components/common';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { EditOutlined, DownOutlined, FilterOutlined } from '@ant-design/icons';
-import { Row, Col, Typography, Tag, Divider, Menu, Dropdown } from 'antd';
-
-const { Title, Text } = Typography;
+import { DownOutlined, FilterOutlined } from '@ant-design/icons';
+import { Row, Col, Menu, Dropdown } from 'antd';
+import Item from './Item';
 
 export default class ItemsScreenView extends Component {
 
@@ -16,55 +14,14 @@ export default class ItemsScreenView extends Component {
         ]
     }
 
+    /**
+     * Method return items list
+     */
     renderItems() {
+        // Looking for data
         if (this.state.data.length === 0) return <p>No items!</p>
 
-        return (
-            <>
-                {
-                    this.state.data.map((item) => (
-                        <Row className="my-2">
-                            <Col span={24} xl={6} xs={24} md={12}>
-                                <AppCard>
-                                    <div className="d-flex justify-content-between">
-                                        <Title level={4}>{item.title} <Tag color={item.tag === 'Product' ? 'purple' : 'blue'}>{item.tag}</Tag></Title>
-                                        <Link to={''}><EditOutlined /></Link>
-                                    </div>
-                                    <Text type="secondary">SKU: {item.SKU}</Text>
-                                    <Divider />
-                                    <Row>
-                                        <Col span={12}>
-                                            <Text type="secondary">HSN Number</Text><br />
-                                            <Text type="secondary">{item.HSN}</Text>
-                                            <div className="vl"></div>
-                                        </Col>
-                                        <Col span={12}>
-                                            <Text type="secondary">Item Price</Text><br />
-                                            <Text type="secondary">{item.price_fmt}</Text>
-                                        </Col>
-                                    </Row>
-                                    <Divider dashed={true} />
-                                    <Row>
-                                        <Col span={8}>
-                                            <Text type="secondary">Tax Rate</Text><br />
-                                            <Text type="secondary">{item.taxRate}{item.ch}</Text>
-                                        </Col>
-                                        <Col span={8}>
-                                            <Text type="secondary">Unit</Text><br />
-                                            <Text type="secondary">{item.unit}</Text>
-                                        </Col>
-                                        <Col span={8}>
-                                            <Text type="secondary">Initial Stock</Text><br />
-                                            <Text type="secondary">{item.initialStock}</Text>
-                                        </Col>
-                                    </Row>
-                                </AppCard>
-                            </Col>
-                        </Row>
-                    ))
-                }
-            </>
-        );
+        return this.state.data.map(item => <Item item={item} />);
     }
 
     render() {
